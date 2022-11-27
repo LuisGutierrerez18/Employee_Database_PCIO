34d3da4ed83a9cbfefb3657348a4368aac2e192f
LuisGutierrerez18 <111073914+LuisGutierrerez18@users.noreply.github.com>
2022-11-27 17:48:10 -0600

    Update InternshipEmployee.h

--- InternshipEmployee.h
+++ InternshipEmployee.h
@@ -1,13 +1,23 @@
 #include "Employee.h"
 
+// class which creates an intership employee with all his/her's characteristics 
 class internshipEmployee()
 {
+    // private variable degree of the intern
     private:
+        //Current year of University
         string degree;
     
+    // Checks if the applicant is studying biotechnology
     public:
         void isElligibleToWork();
-
+        {
+            cout<<"Enter 1 for Yes; 0 for No"
+            cin>>"Are you studying biotechnology?";
+        }
+    
+    // Method which gets in what year of is he/she
+    public:
         void getInternDegree()
         {
             return degree;
