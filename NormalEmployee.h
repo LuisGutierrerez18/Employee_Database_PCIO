a4969ac8946cd41bbec0bf828f5d05dd98921b8d
LuisGutierrerez18 <111073914+LuisGutierrerez18@users.noreply.github.com>
2022-11-27 17:49:02 -0600

    Update NormalEmployee.h

--- NormalEmployee.h
+++ NormalEmployee.h
@@ -1,13 +1,21 @@
 #include "Employee.h"
 
+// Class which diffrentiates a normal employee from an intern and CEO
 class normalEmployee()
 {
+    // String which is dessignated to the academic degree of the normal employee
     private:
         string degree;
     
+    // Marks a project as "Done"
     public:
         void markAsDone();
-
+        {
+            cout<<"Done";
+        }
+    
+    // Method which gets the degree of the normal employee
+    public:
         void getNormalDegree()
         {
             return degree;
