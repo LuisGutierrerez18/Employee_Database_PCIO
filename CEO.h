6c1af3dcb7d13d4d06c6ac05e8d8883b170214f0
LuisGutierrerez18 <111073914+LuisGutierrerez18@users.noreply.github.com>
2022-11-27 17:45:18 -0600

    Update CEO.h

--- CEO.h
+++ CEO.h
@@ -1,18 +1,25 @@
 #include "Employee.h"
+#include "Days.h"
+#include "Project.h"
 
 class CEO()
 {
+    // private variable of the academic degree of the CEO
     private:
         string degree;
     
+    // public method intends to assign project to the other employees with a command 
     public:
         void manageProject()
         {
             cout<<"Assigning prjects">>
         }
-        
-        void getDegree()
+
+    // public getter method wich returns the degree of the CEO of the company    
+    public:     
+     getDegree()
         {
             return degree;
+            
         }
 }
